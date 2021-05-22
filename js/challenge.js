31f29433fd5af49b3049490047d5e8a985578539
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-22 12:39:47 +1000

    Not repeating numbers

--- js/challenge.js
+++ js/challenge.js
@@ -1,11 +1,12 @@
 const counter = document.querySelector('h1#counter')
-
+let timesLiked = 0
 document.addEventListener('DOMContentLoaded',() => {
   counter.innerHTML = 0
   function seconds() {
     let time = 0
     time += 1
     counter.innerHTML = parseInt(counter.innerHTML) + time
+    timesLiked = 0
   }    
   window.setInterval(seconds, 1000)  
 })
@@ -22,23 +23,50 @@ plus.addEventListener('click',() => {
   counter.innerHTML = parseInt(counter.innerHTML) + 1
 })
 
-const li = document.createElement('li')
-const ul = document.querySelector('ul')
+const likesTester = document.createElement('footer')
+document.body.appendChild(likesTester)
+
 const heart = document.querySelector('button#heart')
-
-
-
 heart.addEventListener('click',(event) => {
-  if (document.querySelector('li') === null) {
-    li.innerHTML = counter.innerHTML
-    ul.appendChild(li)
-  }
-  else ul.appendChild(li.cloneNode(true)).innerHTML = counter.innerHTML
+  const li = document.createElement('li')
+  const ul = document.querySelector('ul')
   
-  for (i = 1; i<= document.querySelectorAll('li').length - 1; i++) {
-    if (document.querySelectorAll('li')[i-1].innerHTML===document.querySelectorAll('li')[i].innerHTML) {
+  timesLiked += 1
+  li.innerHTML = `${counter.innerHTML} has been liked ${timesLiked} times`
+  ul.appendChild(li)
+  
+  likesTester.innerHTML = timesLiked
+  
+    
+  for (let i = 1; i<= document.querySelectorAll('li').length - 1; i++) {
+    if (document.querySelectorAll('li').length <= 1) {
+      break
+    }
+    if (numberExtractor(document.querySelectorAll('li')[i-1].innerHTML)===numberExtractor(document.querySelectorAll('li')[i].innerHTML)) {
       document.querySelectorAll('li')[i].remove() 
     }
   }
-  
 })
+
+// const numberExtractor = function (string) {
+//   let number = string.charAt(0)
+//   for (i = 0; i <= string.length -1; i++) {
+//     if (!parseInt(string.charAt(i+1))) {
+//       return number
+//     }
+//     else {
+//       number = number + string.charAt(i+1)
+//     }
+//     return number
+//   }
+// }  
+const numberExtractor = string => {
+  let number = ''
+  for (i = 0; i <= string.length - 1; i++) {
+    if (string.charAt(i) === ' ') {
+      break
+    }
+    number = number + string.charAt(i)
+  }
+  return parseInt(number)
+}
